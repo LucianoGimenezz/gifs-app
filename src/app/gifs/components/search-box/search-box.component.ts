c0884d7836a0424d331d66af82e7b4eea5cc3a44
LucianoGimenezz <luciano.gimenezz21@gmail.com>
2023-08-23 14:31:55 -0300

    some changes

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -4,21 +4,18 @@ import { GifsService } from '../../services/gifs.service';
 @Component({
   selector: 'gifs-search-box',
   templateUrl: './search-box.component.html',
-  styleUrls: ['./search-box.component.css']
+  styleUrls: ['./search-box.component.css'],
 })
 export class SearchBoxComponent {
-
   @ViewChild('tagInput')
-  public tagInput!: ElementRef<HTMLInputElement>
+  public tagInput!: ElementRef<HTMLInputElement>;
 
-  constructor(
-    private readonly _gifsService: GifsService
-  ){}
+  constructor(private readonly _gifsService: GifsService) {}
 
   searchTag() {
-    const newTag = this.tagInput.nativeElement.value
+    const newTag = this.tagInput.nativeElement.value;
 
-    this._gifsService.searchTag(newTag)
-    this.tagInput.nativeElement.value = ''
+    this._gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
   }
 }
